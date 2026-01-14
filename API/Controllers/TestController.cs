33849f99d6ccc262bc19090f0a932bb61e104f2f
Kirill Zlobin <jsdevzk@gmail.com>
2026-01-14 12:52:25 +0500

    Created test controller

---
+++ API/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetHello()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
