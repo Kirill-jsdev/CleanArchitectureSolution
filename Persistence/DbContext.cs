96592f890276ec39637b2a6b1795c2703c76ab60
Kirill Zlobin <jsdevzk@gmail.com>
2026-01-15 23:45:38 +0500

    Added DbContext and installed Postgres Entity Framework adapter to persistence layer

---
+++ Persistence/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) // Calls DbContext's constructor
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
