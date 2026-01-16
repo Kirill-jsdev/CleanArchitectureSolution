25ec93524767bdcce67a4e68d3fb6661e17e53c0
Kirill Zlobin <jsdevzk@gmail.com>
2026-01-16 17:51:37 +0500

    Created Query for getting list of activities from DB using CQRS and MediatR

--- Application/Activities/Queries/GetActivityList.cs
+++ Application/Activities/Queries/GetActivityList.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Application.Activities.DTOs;
+using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 
 namespace Application.Activities.Queries
 {
     internal class GetActivityList
     {
+        public class Query : IRequest<Result<List<ActivityDto>>>
+        {
+        }
+
+        public class Handler(AppDbContext context, IMapper mapper) :
+            IRequestHandler<Query, Result<List<ActivityDto>>>
+        {
+            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activities = await context.Activities
+                    .ProjectTo<ActivityDto>(mapper.ConfigurationProvider)
+                    .ToListAsync(cancellationToken);
+
+                return Result<List<ActivityDto>>.Success(activities);
+            }
+        }
     }
 }
