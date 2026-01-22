3275f4afc1967b6fd2ba1053872898f1ef617607
Kirill Zlobin <jsdevzk@gmail.com>
2026-01-22 14:22:25 +0500

    Created first migration, properly connected database

--- Application/Activities/Queries/GetActivityList.cs
+++ Application/Activities/Queries/GetActivityList.cs
@@ -20,11 +20,20 @@ namespace Application.Activities.Queries
         {
             public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activities = await context.Activities
-                    .ProjectTo<ActivityDto>(mapper.ConfigurationProvider)
-                    .ToListAsync(cancellationToken);
+                try
+                {
+                    var activities = await context.Activities
+                        .ProjectTo<ActivityDto>(mapper.ConfigurationProvider)
+                        .ToListAsync(cancellationToken);
 
-                return Result<List<ActivityDto>>.Success(activities);
+                    return Result<List<ActivityDto>>.Success(activities);
+                }
+                catch (Exception)
+                {
+                    // Log the error here if you have a logger (e.g., _logger.LogError(ex, "Database connection failed"))
+
+                    return Result<List<ActivityDto>>.Failure("Server error: Unable to connect to the database.", 500);
+                }
             }
         }
     }
